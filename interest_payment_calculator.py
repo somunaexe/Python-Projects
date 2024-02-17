70497d3002a2f6570613b38d4c60fb609a30b789
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-02-17 23:59:26 +0000

    commit

---
+++ interest_payment_calculator.py
@@ -0,0 +1,15 @@
+def calculate_payment():
+    print("This is a monthly payment loan calculator")
+    print("")
+
+    principal = float(input("Input the loan amount : "))
+    apr = float(input("Enter the annual interest rate: "))
+    years = int(input("Enter the amount of years: "))
+
+    monthly_interest_rate = apr / 1200
+    amount_of_months = years * 12
+    monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-amount_of_months))
+
+    print("The monthly payment for this loam is: %.2f" % monthly_payment)
+
+calculate_payment()
