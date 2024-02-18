22e1c84398172a4623087d6d640ad893cd2d97ff
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-02-18 01:24:48 +0000

    commit

---
+++ leap_year_checker.py
@@ -0,0 +1,13 @@
+def main(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap Year")
+            else:
+                print("Not a leap year")
+        else:
+            print("Leap year")
+    else:
+        print("Not a leap year")
+
+main(2024)
