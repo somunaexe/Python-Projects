d5fc7563aa172f78387170c29e866245af5d9b63
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-02-18 23:43:11 +0000

    commit

--- resizer_project/imge_resizer.py
+++ resizer_project/imge_resizer.py
@@ -1,9 +1,12 @@
 from PIL import Image
 
-def resize_image():
+def resize_image(size1, size2):
     image = Image.open('Python-Projects/resizer_project/profile_picture.jpg')
     print(f'Current size : {image.size}')
-    resized_image = image.resize((500, 500))
-    resized_image.save('Python-Projects/resizer_project/resized_profile_picture.jpg')
+    resized_image = image.resize((size1, size2))
+    resized_image.save(f'Python-Projects/resizer_project/resized_profile_picture_{str(size1)}_{str(size2)}.jpg')
 
-resize_image()
+inputSize1 = int(input("Enter the width you want the picture to be: "))
+inputSize2 = int(input("Enter the length you want the picture to be: "))
+
+resize_image(inputSize1, inputSize2)
